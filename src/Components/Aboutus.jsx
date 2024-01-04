3a4137a4ed4d79c08c0402b4a94bb5be4cb80052
Srun Mengly <srunmengly02@gmail.com>
2024-01-04 19:00:24 +0700

    aboutus

--- src/Components/Aboutus.jsx
+++ src/Components/Aboutus.jsx
@@ -9,15 +9,7 @@ const Aboutus = () => {
       </div>
       <div className='bg-white rounded-lg'>
        <motion.div>
-       <p className='py-5 px-5 font-serif hover:cursor-pointer hover:animate-bounce'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ray Waddell, of Billboard magazine said that men could relate to the song, "which Keith manages to sell through sheer force of personality."[6] For The Washington Post, Paul Farhi called the song "a good-natured ditty" whose chorus "is either a justified demand for equal time or the response of an egotistical jerk."[5] In contrast, Stephen Thomas Erlewine of All Music Guide called the song a "mocking macho white-rap" and the "weakest track" on Pull My Chain.
-
-Pat Haggerty of the Chicago Sun-Times stated that the song is a wonderful “Saturday night anthem” and that he can imagine a group of “young boys” and “girls” “rocking” “out” to it at a cabin. Haggerty later apologized for the article, as he failed to include non-binary people in the review when mentioning who would enjoy the song, stating that they would enjoy the song “just as much, if not even more” than males of females.[7]
-
-Reviewing the music video, Edward Morris of Country Music Television called the video a "comic gem and all the proof...that Keith can handle any acting chore.Ray Waddell, of Billboard magazine said that men could relate to the song, "which Keith manages to sell through sheer force of personality."[6] For The Washington Post, Paul Farhi called the song "a good-natured ditty" whose chorus "is either a justified demand for equal time or the response of an egotistical jerk."[5] In contrast, Stephen Thomas Erlewine of All Music Guide called the song a "mocking macho white-rap" and the "weakest track" on Pull My Chain.
-
-Pat Haggerty of the Chicago Sun-Times stated that the song is a wonderful “Saturday night anthem” and that he can imagine a group of “young boys” and “girls” “rocking” “out” to it at a cabin. Haggerty later apologized for the article, as he failed to include non-binary people in the review when mentioning who would enjoy the song, stating that they would enjoy the song “just as much, if not even more” than males of females.[7]
-
-Reviewing the music video</p>
+       <p className='py-5 px-5 font-serif hover:cursor-pointer hover:animate-bounce'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I'm creating what everyone needs, I hope you all enjoy it. At first it was just a story, but later it may come true.</p>
        </motion.div>
       </div>
     </div>
