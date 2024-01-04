8c0097cde5f1805fc347f08a6dde928c993c3369
Srun Mengly <srunmengly02@gmail.com>
2024-01-04 19:06:03 +0700

    aboutus.

--- src/Components/Aboutus.jsx
+++ src/Components/Aboutus.jsx
@@ -7,7 +7,7 @@ const Aboutus = () => {
       <div>
         <img src='https://avatars.githubusercontent.com/u/102691647?v=4' className='rounded-lg hover:scale-125 transition duration-300 ease-in-out'></img>
       </div>
-      <div className='bg-white rounded-lg'>
+      <div className='bg-white rounded-lg flex justify-center pt-24'>
        <motion.div>
        <p className='py-5 px-5 font-serif hover:cursor-pointer hover:animate-bounce'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I'm creating what everyone needs, I hope you all enjoy it. At first it was just a story, but later it may come true.</p>
        </motion.div>
